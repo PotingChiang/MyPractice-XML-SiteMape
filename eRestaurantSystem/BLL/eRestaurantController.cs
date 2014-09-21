b5cdf5d68a735127aea474a36b5bca01be116cef
Poting Chiang <poting.chiang@gmail.com>
2014-09-21 15:07:39 -0600

    Update&Setup_eRestaurantDAL&BLL

---
+++ eRestaurantSystem/BLL/eRestaurantController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Name Spaces
+using eRestaurantSystem.Entities;
+using eRestaurantSystem.DAL;
+using System.ComponentModel;
+#endregion Name Spaces 
+namespace eRestaurantSystem.BLL
+{
+    [DataObject]
+    public class eRestaurantController
+    {
+        public List<SpecialEvent> SpecialEvent_List()
+        {
+            using (eRestaurantContext SpecialEventcontext = new eRestaurantContext())
+            {
+                return SpecialEventcontext.SpecialEvents.ToList();
+            }
+        }
+    }
+}
---
+++ eRestaurantSystem/DAL/eRestaurantContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region NameSpaces
+using eRestaurantSystem.Entities;
+using System.Data.Entity;
+#endregion
+
+namespace eRestaurantSystem.DAL
+{
+    internal class eRestaurantContext:DbContext
+    {
+        public eRestaurantContext():base("name=EatIn") {}
+
+        public DbSet<SpecialEvent> SpecialEvents { get; set; }
+    }
+}
